d8e7d28f02c186bf80eaf012f4ac1ce1a40483ef
Tal Shiri <eiopaa@gmail.com>
2013-10-07 21:39:50 -0700

    added state

--- data_puller.py
+++ data_puller.py
@@ -1,5 +1,22 @@
 from github import Github
 from datetime import datetime, timedelta
+import requests
+import options
+import argparse
+import json
+from os import path
+import dateutil.parser
+
+def main():
+    try:
+        opts = options.parse()
+    except(options.ParseError, options.MissingArgumentError):
+        print "Usage: python program.py rainforestapp/GitSatisfaction me@example.org"
+        exit(1)
+
+    puller = DataPuller(opts.username, opts.repository)
+
+    print puller.users
 
 class DataPuller(object):
     def __init__(self, user, repository):
@@ -8,7 +25,47 @@ class DataPuller(object):
         self._gh = Github()
         self._repository = self._gh.get_repo("{}/{}".format(self._user, self._repoistory_name))
         self.users = list(self._repository.get_contributors())
+        self._state = DigestiveState()
 
     def get_issues(self):
-        yesterday = datetime.now() - timedelta(days=1)
-        return self._repository.get_issues(sort='updated', since=yesterday)
+        return self._repository.get_issues(sort='updated', since=self._state.last_sent)
+
+    def process(self):
+        self._state.last_sent = datetime.now()
+
+
+
+class DigestiveState(object):
+    """
+    Fun state stuff that needs to be saved
+    """
+    FILENAME = 'digestive.json'
+    def __init__(self):
+        if path.exists(self.FILENAME):
+            self._data = json.load(open(self.FILENAME))
+        else:
+            self._data = {}
+
+    @property
+    def last_sent(self):
+        """
+        Returns the last sent time. If none exists, it defaults to the last 24 hours.
+        """
+        last_sent = self._data.get('last_sent')
+        if last_sent:
+            return dateutil.parser.parse(last_sent)
+        else:
+            return datetime.now() - timedelta(days=1)
+
+    @last_sent.setter
+    def last_sent(self, value):
+        self._data['last_sent'] = value.isoformat()
+
+    def save(self):
+        """
+        saves state to disk
+        """
+        json.dump(self._data, open(self.FILENAME, 'w'))
+
+if __name__ == '__main__':
+    main()
