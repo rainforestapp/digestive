8354f353003ed2c820989e82cbc176b2e3c73ae1
Tal Shiri <eiopaa@gmail.com>
2013-10-07 22:25:46 -0700

    another test!

--- tests/digestive_test.py
+++ tests/digestive_test.py
@@ -10,8 +10,7 @@ class TestDigestive(MockMixin, unittest.TestCase):
     def make_issue(cls, state='open'):
         return make_mock(state=state)
 
-    def test_get_digest(self):
-        from github import Github
+    def test_get_digest_open_issues(self):
         digestive = Digestive('rainforestapp', 'GitSatisfaction')
         expect(digestive._repository).get_issues.and_return([self.make_issue('open')])
         digest = digestive.get_digest()
@@ -19,6 +18,16 @@ class TestDigestive(MockMixin, unittest.TestCase):
         self.assertEqual(digest.total_opened, 1)
         self.assertEqual(digest.total_closed, 0)
         self.assertEqual(digest.total_issues, 1)
+
+    def test_get_digest_closed_issues(self):
+        digestive = Digestive('rainforestapp', 'GitSatisfaction')
+        expect(digestive._repository).get_issues.and_return([self.make_issue('closed')])
+        digest = digestive.get_digest()
+
+        self.assertEqual(digest.total_opened, 0)
+        self.assertEqual(digest.total_closed, 1)
+        self.assertEqual(digest.total_issues, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
